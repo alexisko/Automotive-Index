57410c980745de537eced0c416caa004c7fc4cf8
Alexis <alexisko@hawaii.edu>
2017-07-22 19:09:19 -1000

    Did last part of exercise ... Indexing on table create

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,17 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+CREATE INDEX q1 ON car_models (model_code);
+
+CREATE INDEX q2 ON car_models (make_code);
+
+CREATE INDEX q3 ON car_models (year);
+
+-- SELECT TIMES AFTER
+-- 1. Time: 1.466 ms VS. Time: 1.667 ms (PREV)
+-- 2. Time: 0.657 ms VS. Time: 0.412 ms (PREV)
+-- 3. Time: 2.122 ms VS. Time: 2.308 ms (PREV)
+-- 4. Time: 82.677 ms VS. Time: 97.758 ms (PREV)
+-- 5. Time: 16.614 ms VS. Time: 17.918 ms (PREV)
