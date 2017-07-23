7ec21df6348ec8ce37b766a7707b1d66d57fe309
Alexis <alexisko@hawaii.edu>
2017-07-22 19:04:20 -1000

    Fixed indexing queries and added times of select statements after creating the indexes

--- indexing.sql
+++ indexing.sql
@@ -16,17 +16,23 @@ SELECT * FROM car_models WHERE year = 2010;
 SELECT COUNT(*) FROM car_models WHERE year = 2010;
 -- Time: 51.364 ms
 
-CREATE INDEX q1 ON car_models (make_title) WHERE make_code = 'LAM';
--- Time: 238.028 ms
+CREATE INDEX q1 ON car_models (model_code);
 
-CREATE INDEX q2 ON car_models (make_title) WHERE make_code = 'GT-R';
--- Time: 49.406 ms
+CREATE INDEX q2 ON car_models (make_code);
 
-CREATE INDEX q3 ON car_models (make_code, model_code, model_title, year) WHERE make_code = 'LAM';
--- Time: 53.185 ms
+CREATE INDEX q3 ON car_models (year);
 
-CREATE INDEX q4 ON car_models (id, make_code, make_title, model_code, model_title, year) WHERE year >= 2010 AND year <= 2015;
--- Time: 114.301 ms
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+-- Time: 1.667 ms
 
-CREATE INDEX q5 ON car_models (id, make_code, make_title, model_code, model_title, year) WHERE year = 2010;
--- Time: 65.626 ms
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'GT-R';
+-- Time: 0.412 ms
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+-- Time: 2.308 ms
+
+SELECT * FROM car_models WHERE year >= 2010 AND year <= 2015;
+-- Time: 97.758 ms
+
+SELECT * FROM car_models WHERE year = 2010;
+-- Time: 17.918 ms
